df5a6c3c6fbe228d91cbc23159549f4dfc0f7595
agrosson <agrosson@gmail.com>
2019-05-28 08:22:42 +0200

    Initial Commit

---
+++ TemplateForGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TemplateForGit
+//
+//  Created by ALEXANDRE GROSSON on 28/05/2019.
+//  Copyright © 2019 GROSSON. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
